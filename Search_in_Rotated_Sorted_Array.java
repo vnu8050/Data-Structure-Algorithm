a8daf6bb65dfdf8f7333a475bd7e565cbfbc7887
_venukulkarni_ <90561526+vnu8050@users.noreply.github.com>
2021-11-19 12:57:41 +0530

    Add files via upload

---
+++ Search_in_Rotated_Sorted_Array.java
@@ -0,0 +1,45 @@
+package DataStructure;
+
+public class Search_in_Rotated_Sorted_Array {
+	 public static int search(int[] nums, int target) {
+         if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int middle = left + (right - left) / 2;
+            if (nums[middle] > nums[right]) {
+                left = middle + 1;
+            }
+            else {
+                right = middle;
+            }
+        }
+        int pivot = left;
+        left = 0;
+        right = nums.length - 1;
+        if (target >= nums[pivot] && target <= nums[right]) {
+            left = pivot;
+        } else {
+            right = pivot;
+        }
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            if (nums[middle] == target) {
+                return middle;
+            } else if (target < nums[middle]) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+        return -1;
+    }
+	 public static void main(String[] args) {
+		int nums[] = {4,5,6,7,0,1,2};
+		int target = 0;
+		System.out.println(search(nums, target));
+	}
+	 
+}
